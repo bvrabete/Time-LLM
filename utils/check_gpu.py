23c94b96bbb4a774ad54d256ef0113b71debdc00
Brad Vrabete <bvrabete@blaxoft.eu>
2024-04-13 11:36:49 +0100

    Minor chnages to try to run on my laptop

---
+++ utils/check_gpu.py
@@ -0,0 +1,5 @@
+import transformers
+
+transformers.utils.import_utils.is_torch_bf16_gpu_available()
+transformers.utils.import_utils.is_torch_cuda_available()
+transformers.utils.import_utils.is_torch_bf16_cpu_available()
